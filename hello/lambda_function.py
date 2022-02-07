688fe5cb345c372f41a53df5589754c3897e16bb
swetageorge <96417593+swetageorge@users.noreply.github.com>
2022-02-07 03:24:04 -0500

    Create lambda_function.py

---
+++ hello/lambda_function.py
@@ -0,0 +1,22 @@
+from github import Github
+
+def lambda_handler(event, context):
+    """Lambda function wrapper
+    Args:
+        event: trigger event dict
+        context: lambda methods and properties
+    Returns:
+        string: greeting response
+    """
+    print('Starting functions\n---------------------------------------------')
+
+    if event["input"] == "Hello":
+
+        return "World"
+    if event["input"] == "Hi":
+
+        return "Hi There!"
+
+    else:
+
+        raise
